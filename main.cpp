60cd32a7bed973cc8cde3f58afb18e9e612d8ab8
kyanma <jennygabrb_25@alu.uabcs.mx>
2026-05-29 10:57:39 -0700

    Implement menu options with switch statement
    Added a switch statement for user options in the menu.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,27 @@ int main()
     cout << "MENU" << endl;
     cout << "BATALLA NAVAL" << endl;
     cout << "BY DIEGO & JENNY" << endl;
-    cout << "\nElige una opci�n:" << endl;
+    cout << "\nElige una opción:" << endl;
     cout << "\n1. JUGAR" << endl;
     cout << "2. REGLAS DEL JUEGO" << endl;
     cout << "3. SALIR DEL JJUEGO" << endl;
 
+     switch (opcion)
+        {
+            case 1:
+                
+                break;
+            
+            case 2:
+                cout << "REGLAS DEL JUEGO" << endl;
+                cout << "Cada jugador tiene un tablero en el que coloca sus barcos.\nLos jugadores se turnan para realizar disparos y tratar de hundir los barcos del oponente." << endl; 
+                cout << "El primer jugador en hundir todos los barcos del oponente gana la partida. " << endl;
+                cout << "BY DIEGO & JENNY" << endl;
+                cout << "\nElige una opción:" << endl;
+                break;
+            
+            case 3:
+                break;
+        }
     return 0;
 }
