7414e7e7cbd063d06adb22b3c60396d18c3710f3
DiegoSalgadoUABCS <dsalgado_25@alu.uabcs.mx>
2026-05-29 11:17:20 -0700

    ciclo do-while para el menu

--- main.cpp
+++ main.cpp
@@ -6,32 +6,35 @@ using namespace std;
 int main()
 {
     int opcion;
+    bool juegoActivo = true;
     system ("chcp 65001 > nul");
-    cout << "MENU" << endl;
-    cout << "BATALLA NAVAL" << endl;
-    cout << "BY DIEGO & JENNY" << endl;
-    cout << "\nElige una opción:" << endl;
-    cout << "\n1. JUGAR" << endl;
-    cout << "2. REGLAS DEL JUEGO" << endl;
-    cout << "3. SALIR DEL JUEGO" << endl;
-    cin >> opcion;
-
-    switch (opcion)
-        {
-            case 1:
+    
+    do
+    {
+        cout << "MENU" << endl;
+        cout << "BATALLA NAVAL" << endl;
+        cout << "BY DIEGO & JENNY" << endl;
+        cout << "\nElige una opción:" << endl;
+        cout << "\n1. JUGAR" << endl;
+        cout << "2. REGLAS DEL JUEGO" << endl;
+        cout << "3. SALIR DEL JUEGO" << endl;
+        cin >> opcion;
+        switch (opcion)
+            {
+                case 1:
+                    
+                    break;
                 
-                break;
-            
-            case 2:
-                cout << "REGLAS DEL JUEGO" << endl;
-                cout << "Cada jugador tiene un tablero en el que coloca sus barcos.\nLos jugadores se turnan para realizar disparos y tratar de hundir los barcos del oponente." << endl; 
-                cout << "El primer jugador en hundir todos los barcos del oponente gana la partida. " << endl;
-                cout << "BY DIEGO & JENNY" << endl;
-                cout << "\nElige una opción:" << endl;
-                break;
-            
-            case 3:
-                break;
-        }
+                case 2:
+                    cout << "REGLAS DEL JUEGO" << endl;
+                    cout << "Cada jugador tiene un tablero en el que coloca sus barcos.\nLos jugadores se turnan para realizar disparos y tratar de hundir los barcos del oponente." << endl; 
+                    cout << "El primer jugador en hundir todos los barcos del oponente gana la partida." << endl;
+                    cout << "----------------------------------------------------------------------------" << endl;
+                    break;
+                
+                case 3:
+                    return 0;
+            }
+        } while (juegoActivo == true);
     return 0;
 }
