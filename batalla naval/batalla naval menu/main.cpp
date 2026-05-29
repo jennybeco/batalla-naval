b7f76872bcf23721f50f237eb25a2a5b99b013f4
kyanma <jennygabrb_25@alu.uabcs.mx>
2026-05-29 10:51:23 -0700

    Add option handling to main menu

--- batalla naval/batalla naval menu/main.cpp
+++ batalla naval/batalla naval menu/main.cpp
@@ -7,10 +7,20 @@ int main()
     cout << "MENU" << endl;
     cout << "BATALLA NAVAL" << endl;
     cout << "BY DIEGO & JENNY" << endl;
-    cout << "\nElige una opción:" << endl;
+    cout << "\nElige una opciÃ³n:" << endl;
     cout << "\n1. JUGAR" << endl;
     cout << "2. REGLAS DEL JUEGO" << endl;
     cout << "3. SALIR DEL JJUEGO" << endl;
+    cin >> opcion;
 
+    switch (opcion)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
     return 0;
 }
