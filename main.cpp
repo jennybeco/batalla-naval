19c321b1b310f6aa225f5af6c70292612bf56cb4
DiegoSalgadoUABCS <dsalgado_25@alu.uabcs.mx>
2026-06-01 19:25:39 -0700

    Pantalla de inicio // Reestructuracion menu

--- main.cpp
+++ main.cpp
@@ -3,38 +3,98 @@
 
 using namespace std;
 
+int menu(), juego();
+void pantallaInicio(), limpiarConsola(), reglas();
+string cualquierBoton;
+
 int main()
 {
-    int opcion;
     bool juegoActivo = true;
     system ("chcp 65001 > nul");
-    
+    pantallaInicio();
     do
     {
-        cout << "MENU" << endl;
-        cout << "BATALLA NAVAL" << endl;
-        cout << "BY DIEGO & JENNY" << endl;
-        cout << "\nElige una opción:" << endl;
-        cout << "\n1. JUGAR" << endl;
-        cout << "2. REGLAS DEL JUEGO" << endl;
-        cout << "3. SALIR DEL JUEGO" << endl;
-        cin >> opcion;
-        switch (opcion)
-            {
-                case 1:
+        switch (menu())
+        {
+            case 1:
+                        
+                break;
                     
-                    break;
-                
-                case 2:
-                    cout << "REGLAS DEL JUEGO" << endl;
-                    cout << "Cada jugador tiene un tablero en el que coloca sus barcos.\nLos jugadores se turnan para realizar disparos y tratar de hundir los barcos del oponente." << endl; 
-                    cout << "El primer jugador en hundir todos los barcos del oponente gana la partida." << endl;
-                    cout << "----------------------------------------------------------------------------" << endl;
-                    break;
-                
-                case 3:
-                    juegoActivo = false;
-            }
-        } while (juegoActivo == true);
-    return 0;
+            case 2:
+                reglas();
+                break;
+                    
+            case 3:
+            juegoActivo = false;
+        }
+    }while (juegoActivo == true);
+    
+
+}
+
+int menu()
+{
+    int opcion;
+    cout << "MENU PRINCIPAL" << endl;
+    cout << "\nELIGE UNA OPCION:" << endl;
+    cout << "1. JUGAR" << endl;
+    cout << "2. REGLAS DEL JUEGO" << endl;
+    cout << "3. SALIR DEL JUEGO" << endl;
+    cin >> opcion;
+       
+    return opcion;
+}
+
+void pantallaInicio()
+{
+    cout << "BATALLA NAVAL POR JENNY Y DIEGO " << endl;
+    cout << R"(                                
+                                                                =.                                                                  
+                     ..:..-%*..                                 =.                                  
+            ..*#*:.....*#+:-*++..                           .:-.=.                                  
+          ...*+#=+**=:..:++=.=+*=...                        .:-.-:                                  
+          ..#+=*#+++++#*:.++*:.+=*:...                    ....=.::....                              
+        ..:#====#+++++++++%*#+*.-+++...                   .*%%%%%%%%..                              
+        .:#+====+*+++++++++++*##+.++*-...                 .=######%%:.                              
+       .-*=======#+++++++++++++++#+=*+*.....              ..-######-..                              
+     ..-#========+#+++++++++++++++++*%**++=*#:..         .::=*+++=+-..                              
+     .=*==========*+++++++++++++++++++++#%++===#=.     ...+@@@@@@@%%*..                             
+    .:#===========+#+++++*#*#*%++++++++++++*##===*:.   .+++*********#-.                             
+     ..*+==========#%%%#*++++++++++++++++++++++##**...  :********++=+-...                           
+      ..=+========+*%*****%%#*+++++++++++++++++++++%+:...:*+==++++*++*#+-...                        
+        .-#======*#*##**********%@#+++++++++++++++++++*%=+%*=+#%@#*++%*===+.                        
+        ..:#====*****%***************##%#+++++++++++++++++*#+=+++*###**=++:...                      
+          ..+++#*****#%*******************#%##*++++++++++++++*##+=+#*+******=.                      
+            .-%*******%*************************#%%**++++++++++++#%#=+*#*++#+..                     
+             .:%******##*******#%%%*******************#%@*+++++++++++%%===+#*:..                    
+              ..+#*****%********%@%#************************#%%*++++++++*#***+*.......              
+              ...-#*****%******%#%*%%****************************#%##**+**++##**+=+*=.....          
+                 ..#****##*******##************************************#%%**++++**+++%###:.....     
+....................+****%***************************************************#%%*++*%#....+%%*..    
+#*****************###%%**#********************************************************##%%##*:-.......  
+*********************+++********************############################%%%%%%%%########%@%-===++==:
+*****************************************************************************************+++++++++++
+****************************************************************************************************)" << endl;
+cout << "INGRESE CUALQUIER BOTON: ";
+cin >> cualquierBoton;
+limpiarConsola();
+return;
+}
+
+void limpiarConsola()
+{
+    cout << "\033[2J\033[1;1H";
+    return;
+}
+
+void reglas()
+{
+    cout << "REGLAS DEL JUEGO" << endl;
+    cout << "Cada jugador tiene un tablero en el que coloca sus barcos.\nLos jugadores se turnan para realizar disparos y tratar de hundir los barcos del oponente." << endl; 
+    cout << "El primer jugador en hundir todos los barcos del oponente gana la partida." << endl;
+    cout << "----------------------------------------------------------------------------" << endl;
+    cout << "INGRESE CUALQUIER BOTON PARA REGRESAR AL MENU: ";
+    cin >> cualquierBoton;
+    limpiarConsola();
+    return;
 }
